d5bfb94030b37ac1ad9edb67606fafed80e16142
kemister85 <karlkemistersheppard@gmail.com>
2022-09-23 12:25:46 +1000

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :blogs
   get 'render/index'
+  get 'users/sign_in'
+  get 'user/sign_up'
  
   # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
   root 'render#index'end
